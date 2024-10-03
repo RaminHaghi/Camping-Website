f3defc3a4c6f950dfe8b26b4e801440f8eeafd13
Ramin Haghi <raminhaghi33@gmail.com>
2024-10-03 15:20:26 +0330

    feat nav mobile menu

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,15 @@
 const navBtn = document.querySelector(".navbar__btn")
+const menuOpen = document.querySelector(".nav-mobile")
+
 let navOpen = false;
 navBtn.addEventListener("click",function () {
     if (navOpen) {
         navBtn.classList.remove("navbar__btn--open")
+        menuOpen.classList.remove("nav-mobile--open")
         navOpen = false
     } else {
         navBtn.classList.add("navbar__btn--open")
+        menuOpen.classList.add("nav-mobile--open")
         navOpen = true
     }
 })
